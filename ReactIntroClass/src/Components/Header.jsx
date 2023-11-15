926c3baca0f283509938702bb65680f3267e579c
rashedabdullah <maarashed40@gmail.com>
2023-11-15 21:26:05 +0600

    react

---
+++ ReactIntroClass/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <ul className='flex contents'>
+            <li>Contact</li>
+            <li>About Me</li>
+            <li>Projects</li>
+            <li>Github</li>
+            <li>Linkedin</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header;
