3e7db309ae5f26581696aa654fd43f1b2123c9c0
rashedabdullah <maarashed40@gmail.com>
2023-11-25 15:57:05 +0600

    ContextAPI

---
+++ ContextAPI/src/Components/Child1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nati from './Nati';
+
+const Child1 = () => {
+  return (
+    <div>
+        <Nati />
+    </div>
+  )
+}
+
+export default Child1;
---
+++ ContextAPI/src/Components/Nati.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { CounterContext } from './../App';
+
+const Nati = () => {
+    const {count, setCount} = useContext(CounterContext);
+  return (
+    <div style={{textAlign: "center"}}>
+      <p>{count}</p>
+      <button style={{height: "40px"}} onClick={()=>setCount(count + 1)}>increment</button>
+    </div>
+  )
+}
+
+export default Nati;
